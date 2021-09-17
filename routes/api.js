238f21c5625146ba6b56fe23f07d61d13f1febf5
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-09-17 19:48:03 +0530

    push

---
+++ routes/api.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const createHash = require('hash-generator');
+
+const {MongoClient} = require("mongodb");
+
+const client = new MongoClient(process.env.REACT_APP_MONGO_URI,{useUnifiedTopology:true,useNewUrlParser:true});
+
+
+router.post('/sign-in-user',(req,res)=>{
+    async function signInUser(){
+        try{
+            await client.connect()
+            const database=client.db("UsersDB")
+            const userLoginInfo=req.body
+            const collection=database.collection("Users")
+            
+            
+            if(userLoginInfo.email)
+            {const userExistsCountEmail=await collection.count({email: userLoginInfo.email,password: userLoginInfo.password},{limit:1})
+            if(userExistsCountEmail===1){
+                const hash=createHash(16)
+                const myQuery={email: userLoginInfo.email}
+                const newValues={$set:{token: hash}}
+                const result=await collection.updateOne(myQuery,newValues)
+                
+                return res.json({token:hash,error:''})
+            }
+            else{
+                return res.json({token:'',error:"User doesn't exist!"})
+            }
+            }
+            else if(userLoginInfo.username)   
+            {const userExistsCountUsername=await collection.count({username: userLoginInfo.username,password: userLoginInfo.password},{limit:1})
+                if(userExistsCountUsername===1){
+                    const hash=createHash(16)
+                    const myQuery={username: userLoginInfo.username}
+                    const newValues={$set:{token: hash}}
+                    const result=await collection.updateOne(myQuery,newValues)
+                    return res.json({token:hash,error:""})
+                }
+                else{
+                    return res.json({token:'',error:"User doesn't exist!"})
+                }
+            
+            }
+
+        }
+        catch(err){
+            return res.json({error: err.message,token: ''})
+        }
+    }
+    signInUser()
+});
+
+router.post('/sign-up-user',(req,res)=>{
+    async function signUpUser(){
+        try{
+            await client.connect()
+            const database=client.db("UsersDB")
+            const userLoginInfo=req.body
+            const collection=database.collection(`Users`)
+            const alreadyExists=await collection.count({email : userLoginInfo.email},{limit:1})
+            const alreadyExistsUsername=await collection.count({username : userLoginInfo.username},{limit:1})
+            if(alreadyExists===1 || alreadyExistsUsername===1){
+                return res.json({error: 'User with this Email/Username Already Exists!',refNo:null})
+            }
+            const hash=createHash(16)
+            const finalUserInfo={...userLoginInfo,
+            token:hash}
+            const result = await collection.insertOne(finalUserInfo);
+            return res.json({refNo: hash, error: null})
+        }
+        catch(err){
+            return res.json({error: err.message, refNo: null})
+        }
+    }
+    signUpUser()
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const path = require('path');
+const router = express.Router();
+
+const cors = require('cors');
+const {urlencoded} = require('body-parser');
+
+const routes = require('./routes/api')
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.REACT_APP_MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected',()=>{
+    console.log('Mongoose is connected!')
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('tiny'));
+
+app.use('/',routes);
+
+app.listen(PORT,console.log(`Server is running at ${PORT}`));
